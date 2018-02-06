e4ac2dfab39f4744bbb9a466305d13ab1fb22326
Filipe Marins <filipe@goldman-marketing.com>
2018-02-06 10:14:48 -0300

    Componentes pais e filhos

--- ex/family.jsx
+++ ex/family.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { childrenWithProps } from '../utils/reactUtils'
 
 export default props => (
     <div>
-        <h1>Família</h1>
-        {React.cloneElement(props.children, props)}
+        <h1>Família</h1>    
+        {/* Só funciona para um elemento 
+            {React.cloneElement(props.children, {...props})}
+        */}
+        { /* multiplos
+        { React.Children.map(props.children, child => React.cloneElement(child, {...props})) }
+        */}
+        { childrenWithProps(props.children, props) }
     </div>
 )
